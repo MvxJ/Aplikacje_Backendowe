4fea83b1b89d6ea32e366e0d6e4f3fc837a0abe2
MvxJ <58326571+MvxJ@users.noreply.github.com>
2022-05-23 19:57:44 +0200

    Create base project for final project

---
+++ gymworkouts/gymworkouts/src/main/java/com/gymworkouts/gymworkouts/GymWorkoutsApplication.java
@@ -0,0 +1,13 @@
+package com.gymworkouts.gymworkouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWorkoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymWorkoutsApplication.class, args);
+	}
+
+}
